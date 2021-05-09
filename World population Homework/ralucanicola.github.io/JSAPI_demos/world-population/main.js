89ba7dcc971d9c0e374428f5c840fb76429a573e
allaboutplanets <78298766+allaboutplanets@users.noreply.github.com>
2021-05-09 21:28:55 +0530

    Popup 1

--- World population Homework/ralucanicola.github.io/JSAPI_demos/world-population/main.js
+++ World population Homework/ralucanicola.github.io/JSAPI_demos/world-population/main.js
@@ -238,3 +238,57 @@ require([
     }
     setPadding();
 });
+
+
+Splitting();
+
+
+
+
+$(function() {
+    $('.pop-up').hide();
+    $('.pop-up').fadeIn(1000);
+    
+        $('.close-button').click(function (e) { 
+  
+        $('.pop-up').fadeOut(700);
+        $('#overlay').removeClass('blur-in');
+        $('#overlay').addClass('blur-out');
+        e.stopPropagation();
+          
+      });
+   });
+
+
+
+
+
+
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector(".trigger");
+const closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
+
+
+
+
+
+$(document).ready(function(){
+    $('#onload-modal').modal({
+        fadeDuration: 250
+      });
+    });
